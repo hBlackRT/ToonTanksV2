4660c4fb9a115e2f3c7298c5bf48afc88fa4ec97
Rennan <79371839+vKurode@users.noreply.github.com>
2022-03-28 15:09:25 -0300

    Add files via upload

---
+++ Tank.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+
+ATank::ATank()
+{
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    SpringArmComponent->SetupAttachment(RootComponent);
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    CameraComponent->SetupAttachment(SpringArmComponent);
+
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+    PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+
+    PlayerInputComponent->BindAction(TEXT("Fire"), IE_Pressed, this, &ATank::Fire);
+}
+
+void ATank::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if(TankPlayerController)
+    {
+        FHitResult HitResult;
+        TankPlayerController->GetHitResultUnderCursor(
+            ECollisionChannel::ECC_Visibility,
+            false,
+            HitResult);
+
+        RotateTurret(HitResult.ImpactPoint);
+    }
+}
+
+void ATank::HandleDestruction()
+{
+    Super::HandleDestruction();
+    SetActorHiddenInGame(true);
+    SetActorTickEnabled(false);
+    bAlive = false;
+}
+
+void ATank::BeginPlay()
+{
+    Super::BeginPlay();
+
+    TankPlayerController = Cast<APlayerController>(GetController());
+}
+
+void ATank::Move(float Value)
+{
+    FVector DeltaLocation(Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this), 0.f, 0.f);
+    AddActorLocalOffset(DeltaLocation, true);
+    
+}
+
+void ATank::Turn(float Value)
+{
+    FRotator DeltaRotation(0.f, Value * TurnRate * UGameplayStatics::GetWorldDeltaSeconds(this), 0.f);
+    AddActorLocalRotation(DeltaRotation, true);
+}
